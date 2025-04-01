58c196a0de27562273689db04b8b1cd57986d7a5
Yusuf cengiz <yc166959@gmail.com>
2025-04-01 11:22:33 +0300

    f

---
+++ constants/index.js
@@ -0,0 +1,93 @@
+{
+  "pages": [
+    {
+      "id": 1,
+      "fileName": "anaSayfa.html",
+      "iconLink": "https://example.com/icons/home-icon.png"
+    },
+    {
+      "id": 2,
+      "fileName": "dogC.html",
+      "iconLink": "https://example.com/icons/dog-icon.png"
+    },
+    {
+      "id": 3,
+      "fileName": "memCard.html",
+      "iconLink": "https://example.com/icons/card-icon.png"
+    },
+    {
+      "id": 4,
+      "fileName": "atisOyunu.html",
+      "iconLink": "https://example.com/icons/game-icon.png"
+    },
+    {
+      "id": 5,
+      "fileName": "chat.html",
+      "iconLink": "https://example.com/icons/game"
+    },
+    {
+      "id": 6,
+      "fileName": "tiklama.html",
+      "iconLink": "https://example.com/icons/game"
+    },
+{
+      "id": 7,
+      "fileName": "tvAnimation.html",
+      "iconLink": "https://example.com/icons/game"
+    },
+{
+      "id": 8,
+      "fileName": "cubeAnimation.html",
+      "iconLink": "https://example.com/icons/game"
+    }
+,
+{
+      "id": 9,
+      "fileName": "moonAnimation.html",
+      "iconLink": "https://example.com/icons/game"
+    },
+     {
+      "id": 10,
+      "fileName": "cube3.html",
+      "iconLink": "https://example.com/icons/game"
+    },
+{
+      "id": 11,
+      "fileName": "scary-mirror.html",
+      "iconLink": "https://example.com/icons/game"
+    },
+{
+      "id": 12,
+      "fileName": "fis.html",
+      "iconLink": "https://example.com/icons/game"
+    },{
+      "id": 12,
+      "fileName": "kutuYakalama.html",
+      "iconLink": "https://example.com/icons/game"
+    },{
+      "id": 13,
+      "fileName": "kazan.html",
+      "iconLink": "https://example.com/icons/game"
+    },{
+      "id": 13,
+      "fileName": "balonPatlatma.html",
+      "iconLink": "https://example.com/icons/game"
+    },{
+      "id": 14,
+      "fileName": "kutuKirma.html",
+      "iconLink": "https://example.com/icon"
+    },{
+      "id": 15,
+      "fileName": "doodleJump.html",
+      "iconLink": "https://example.com/icon"
+    },{
+      "id": 16,
+      "fileName": "kebap.html",
+      "iconLink": "https://example.com/icon"
+    },{
+      "id": 17,
+      "fileName": "gam.html",
+      "iconLink": "https://example.com/icon"
+    }
+  ]
+}
---
+++ sp
@@ -0,0 +1,15 @@
+Your public key has been saved in /data/data/com.termux/files/usr/etc/ssh/ssh_host_ed25519_key.pub
+The key fingerprint is:
+SHA256:oyF1mSFYr0lQW8OzPU5SyXJarSkVzJ2utFZKkjJ3F3E u0_a161@localhost
+The key's randomart image is:
++--[ED25519 256]--+
+|    .++.++.=.oE  |
+|    .. =+=X =.   |
+|      + =% + .   |
+|     oo+O X +    |
+|    . ++SX B     |
+|     . o .*      |
+|      .  .       |
+|                 |
+|                 |
++----[SHA256]-----+
